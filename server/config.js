e27fec157081e2eeb52867853458ba631299f318
Joshua <9924769+ak99372@users.noreply.github.com>
2018-02-05 11:05:05 -0800

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
 //.gitignore
 module.exports = {
   google: {
-    clientID: "759233560205-bvanqpl56f1hrfjsvk8fe1jt78pfvops.apps.googleusercontent.com",
-    clientSecret: "eLrWtWnrRwFNHLi7VXUKTuaB"
+    clientID: "",
+    clientSecret: ""
   },
   mongodb: {
-    dbURl: "mongodb://admin:Welcome1@ds121268.mlab.com:21268/zeenau-db"
+    dbURl: ""
   },
   session: {
     cookieKey: 'SomethingIsAwesomeIGuess'
